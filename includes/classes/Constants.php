015bab8c3e831491b7d7794ad1d584d2ce613c87
Jacques Massa <jacquesmassa@macbooks-mbp.attlocal.net>
2018-05-15 04:39:24 -0500

    move constants into separate class file

---
+++ includes/classes/Constants.php
@@ -0,0 +1,12 @@
+<?php 
+	class Constants{
+		public static $passwordsDoNotMatch = "Your passwords don't match";
+		public static $passwordNotAlphanumeric = "Your password can only contain letter and numbers";
+		public static $passwordCharacters = "Your password must be between 5 and 30 characters";
+		public static $emailsDoNotMatch = "Your emails don't match";
+		public static $emailInvalid = "Your email is invalid";
+		public static $lastNameCharacters = "our last name must be between 2 and 25 character";
+		public static $fistNameCharacters = "Your first name must be between 2 and 25 character";
+		public static $usernameCharacters = "YYour username must be between 5 and 25 character";
+	}
+ ?>
--- register.php
+++ register.php
@@ -1,10 +1,17 @@
 <?php 
 	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
 
 	$account = new Account();
 
 	include("includes/handlers/register-handler.php");
 	//include("includes/handlers/login-handler.php");
+
+	function getInputValue($name){
+		if(isset($_POST[$name])){
+			echo $_POST[$name];
+		}
+	}
 
 	?>
 
@@ -20,7 +27,7 @@
 
 			<p>
 				<label for="loginUserName">Username</label>
-				<input id="loginUserName" type="text" name="loginUserName" placeholder="eg. bartsimpson" required>
+				<input id="loginUserName" type="text" name="loginUserName" placeholder="eg. bartsimpson" required> 
 			</p>
 			<p>
 				<label for="loginPassword">Password</label>
@@ -34,32 +41,39 @@
 			<h2>Create a Free Account</h2>
 
 			<p>
-				<?php echo $account->getError("Your username must be between 5 and 25 character"); ?>
+				<?php echo $account->getError(Constants::$usernameCharacters); ?>
 				<label for="username">Username</label>
-				<input id="username" type="text" name="username" placeholder="eg. bartsimpson" required>
+				<input id="username" type="text" name="username" placeholder="eg. bartsimpson" value="<?php getInputValue('username') ?>" required>
 			</p>
 
 			<p>
+				<?php echo $account->getError(Constants::$fistNameCharacters); ?>
 				<label for="firstname"> First Name</label>
-				<input id="firstname" type="text" name="firstname" placeholder="eg. bart" required>
+				<input id="firstname" type="text" name="firstname" placeholder="eg. bart" value="<?php getInputValue('firstname') ?>" required>
 			</p>
 
 			<p>
+				<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 				<label for="lastname">Last Name</label>
-				<input id="lastname" type="text" name="lastname" placeholder="eg. Simpson" required>
+				<input id="lastname" type="text" name="lastname" placeholder="eg. Simpson" value="<?php getInputValue('lastname') ?>"required>
 			</p>
 
 			<p>
+				<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+				<?php echo $account->getError(Constants::$emailInvalid); ?>
 				<label for="email">Email</label>
-				<input id="email" type="email" name="email" placeholder="eg. bartsimpson@gmail.com" required>
+				<input id="email" type="email" name="email" placeholder="eg. bartsimpson@gmail.com" value="<?php getInputValue('email') ?>"required>
 			</p>
 
 			<p>
 				<label for="email2">Email</label>
-				<input id="email2" type="text" name="email2" placeholder="eg. bartsimpson@gmail.com" required>
+				<input id="email2" type="text" name="email2" placeholder="eg. bartsimpson@gmail.com" value="<?php getInputValue('email2') ?>"required>
 			</p>
 
 			<p>
+				<?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+				<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+				<?php echo $account->getError(Constants::$passwordCharacters); ?>
 				<label for="password">Password</label>
 				<input id="password" type="password" name="password" placeholder="Your password" required>
 			</p>
