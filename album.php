830d1640ec564c7a2f4a1c5c6c3cdd65ca2ef4b9
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-18 17:36:12 -0500

    add Song class

--- album.php
+++ album.php
@@ -19,7 +19,8 @@
 
     <div class="righSection">
          <h2><?php echo $album->getTitle(); ?></h2>
-         <span>By <?php echo $artist->getName();?></span>
+         <p>By <?php echo $artist->getName();?></p>
+         <p> <?php echo $album->getNumberOfSongs();?> Songs</p>
     </div>
 </div>
 
