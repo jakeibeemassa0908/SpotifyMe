81ad0a3ed6ea643b5e8b10fea793e5e9cbaede6c
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-19 16:48:31 -0500

    complete Song class

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -25,7 +25,25 @@
             $this->genre=$this->mysqliData['genre'];
             $this->duration=$this->mysqliData['duration'];
             $this->path=$this->mysqliData['path'];
+        }
 
+        public function getTitle(){
+            return $this->title; 
+        }
+        public function getArtist(){
+            return new Artist($this->con,$this->artistId); 
+        }
+        public function getAlbum(){
+            return new Album($this->con, $this->albumId);
+        }
+        public function getGenre(){
+            return $this->genre; 
+        }
+        public function getDuration(){
+            return $this->duration; 
+        }
+        public function getPath(){
+            return $this->path; 
         }
 
     }
