5f7deea4b88fbd4a87932948c68d996c3fab92dc
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-19 17:42:40 -0500

    style track information section

--- album.php
+++ album.php
@@ -24,4 +24,42 @@
     </div>
 </div>
 
+<div class ="tracklistContainer">
+    <ul class="tracklist">
+        <?php 
+            $songIdArray = $album->getSongId();
+            $i = 1;
+            foreach($songIdArray as $songId){
+                  
+                $albumSong = new Song($con,$songId);
+                $albumArtist = $albumSong->getArtist();
+                
+                echo "
+                    <li class ='tracklistRow'> 
+                        <div class='trackCount'>
+                            <img class='play' src='assets/images/icons/play-white.png'>
+                            <span class='trackNumber'>$i</span>
+                        </div>
+
+                        <div class ='trackInfo'>
+                            <span class='trackName'>".$albumSong->getTitle()."</span>
+                            <span class='artistName'>".$albumArtist->getName()."</span>
+                        </div>
+
+                        <div class ='trackOptions'>
+                            <img class='optionButton' src='assets/images/icons/more.png'>
+                        </div>
+
+                        <div class='trackDuration'>
+                            <span class='duration'>". $albumSong->getDuration()."</span>
+                        </div>
+                    </li>
+                ";
+
+                $i = $i+1;
+            }
+        ?>
+    </ul>
+</div>
+
 <?php include("includes/footer.php")?>
