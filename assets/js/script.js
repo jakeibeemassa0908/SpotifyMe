4079e638ecbefaacf92456cbef91d4483386076f
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-22 10:46:55 -0500

    add duration to timer and format output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds){
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time/60); //Rounds down
+    var seconds = time - minutes*60;
+
+    var extraZero = seconds < 10 ? "0" :"";
+
+    return minutes +":"+extraZero+seconds;
+}
+
 function Audio(){
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
+
+    this.audio.addEventListener("canplay",function(){
+        //this refers to the object that the event was called on
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    });
 
     this.setTrack = function (track) {
         this.currentlyPlaying = track;
