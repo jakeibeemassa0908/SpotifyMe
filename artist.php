18544fb80feaa56256e4315629cdd21d15371922
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-24 10:02:01 -0500

    show most played songs in artist page

--- artist.php
+++ artist.php
@@ -10,13 +10,59 @@
     $artist = new Artist($con,$artistId);
 ?>
 
-<div class="entityInfo">
+<div class="entityInfo borderBottom">
     <div class="centerSection">
         <div class = "artistInfo">
             <h1 class = "artistName"><?php echo $artist->getName()?></h1>
             <div class ="headerButtons">
-                <button class="button">Play</button>
+                <button class="button green">PLAY</button>
             </div>
         </div>
     </div>
+</div>
+
+
+<div class ="tracklistContainer borderBottom">
+    <ul class="tracklist">
+        <?php 
+            $songIdArray = $artist->getSongIds();
+            $i = 1;
+            foreach($songIdArray as $songId){
+                //only show 5 songs
+                if($i>5){
+                    break;
+                }
+                $albumSong = new Song($con,$songId);
+                $albumArtist = $albumSong->getArtist();
+                
+                echo "
+                    <li class ='tracklistRow'> 
+                        <div class='trackCount'>
+                            <img class='play' src='assets/images/icons/play-white.png' onclick = 'setTrack(\"". $albumSong->getId(). "\", temporaryPlaylist,true)'>
+                            <span class='trackNumber'>$i</span>
+                        </div>
+
+                        <div class ='trackInfo'>
+                            <span class='trackName'>".$albumSong->getTitle()."</span>
+                            <span class='artistName'>".$albumArtist->getName()."</span>
+                        </div>
+
+                        <div class ='trackOptions'>
+                            <img class='optionButton' src='assets/images/icons/more.png'>
+                        </div>
+
+                        <div class='trackDuration'>
+                            <span class='duration'>". $albumSong->getDuration()."</span>
+                        </div>
+                    </li>
+                ";
+
+                $i = $i+1;
+            }
+        ?>
+        <script>
+            var tempSongIds = '<?php echo json_encode($songIdArray);?>';
+            temporaryPlaylist = JSON.parse(tempSongIds);
+        </script>
+    </ul>
 </div>
