20659b8b0c0a0bf9ddd4ef0e642549cd991c5353
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-26 20:37:55 -0500

    create new playlist page

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php 
+    include("includes/includedFiles.php"); 
+?>
+
+<div class ="playlistContainer">
+    <div class = "gridViewContainer">
+        <h2>PLAYLIST</h2>
+        <div class="buttonItems">
+            <button class="button green" onclick="createPlaylist('jac')">NEW PLAYLIST</button>
+        </div>
+    </div>
+</div>
