6872521d1203376a6109ce97ae97a83b1306f277
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-28 21:58:58 -0500

    create User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -2,9 +2,16 @@
     //check if it was sent with ajax. every ajax request includes this
     if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
         include ("includes/config.php");
+        include("includes/classes/User.php");
         include("includes/classes/Artist.php");
         include("includes/classes/Album.php");
         include("includes/classes/Song.php");
+        if(isset($_GET['userLoggedIn'])){
+            $userLoggedIn = new User($con,$_GET['userLoggedIn']);
+        } else {
+              echo "username variable was not passed into the page";
+              exit();
+        }
         
     }else{
         include("includes/header.php");
