014e5bb78a50725ffbdc16ef55614063867b4151
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-29 23:34:42 -0500

    output user playlist from db

--- yourMusic.php
+++ yourMusic.php
@@ -8,5 +8,30 @@
         <div class="buttonItems">
             <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
         </div>
+        
+        <?php 
+            $username = $userLoggedIn->getUsername();
+            $playlistQuery = mysqli_query($con,"SELECT * FROM playlist WHERE owner = '$username'");
+
+            if(mysqli_num_rows($playlistQuery)==0){
+                echo "<span class ='noResults'>You don't have any playlist yet</span>";
+            }
+
+			while($row = mysqli_fetch_array($playlistQuery)){
+
+				echo " 
+                <div class='gridViewItem'>
+                    <div class = 'playlistImage'>
+                        <img src = 'assets/images/icons/playlist.png'>
+                    </div>
+                    <div class='gridViewInfo'>"
+                        .$row['name']."
+                    </div>
+				</div>";
+			}
+		?>
+
+
+
     </div>
 </div>
