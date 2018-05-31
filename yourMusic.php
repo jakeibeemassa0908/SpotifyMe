e83b81b6c833329cb91f3c0f9c3aa7780432041d
jakeibeemassa0908 <jac.massa0908@gmail.com>
2018-05-31 00:05:10 -0500

    style playlist and create link to playlist page

--- yourMusic.php
+++ yourMusic.php
@@ -20,7 +20,7 @@
 			while($row = mysqli_fetch_array($playlistQuery)){
                 $playlist = new Playlist($con,$row); 
 				echo " 
-                <div class='gridViewItem'>
+                <div class='gridViewItem' role = 'link' tabindex = '0' onclick = 'openPage(\"playlist.php?id=".$playlist->getId()."\")'>
                     <div class = 'playlistImage'>
                         <img src = 'assets/images/icons/playlist.png'>
                     </div>
